8327214e71f62c44923d9e146dc1230ff5a9d630
massfbr <massfbr@gmail.com>
2019-04-02 21:10:58 +0200

    deploy in prod 2/4

--- frontend/auth-services/src/main/java/it/csi/frontend/bifisic/frontend/auth/controller/HttpBasicAuthServiceController.java
+++ frontend/auth-services/src/main/java/it/csi/frontend/bifisic/frontend/auth/controller/HttpBasicAuthServiceController.java
@@ -38,6 +38,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -46,6 +47,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
@@ -63,6 +65,8 @@ import it.csi.frontend.bifisic.frontend.auth.utils.LogFormatter;
 import it.csi.frontend.bifisic.frontend.auth.vo.HttpBasicAuthUserPermissionVO;
 import it.csi.frontend.bifisic.frontend.auth.vo.HttpBasicRole;
 import it.csi.frontend.bifisic.frontend.auth.vo.HttpBasicService;
+import java.util.Arrays;
+import java.util.HashMap;
 
 /**
  */
@@ -329,7 +333,7 @@ public final class HttpBasicAuthServiceController {
 	        return new ResponseEntity<Object>(msg,HttpStatus.valueOf(statusCode));
 		} catch (RestClientException e) {
 			String msg = e.getMessage();
-	        return new ResponseEntity<Object>(msg + "\noriginal status: "+responseEntity!=null?responseEntity.getStatusCode():" unavailable",HttpStatus.INTERNAL_SERVER_ERROR);
+	        return new ResponseEntity<Object>(msg + "\nmessage: "+e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 
 	    String layerUrl = getHttpBasicServiceProperties().getProperty("layerURL");
@@ -368,5 +372,228 @@ public final class HttpBasicAuthServiceController {
         return new ResponseEntity<Object>(userPojo,responseEntityLayer.getStatusCode());
 
 	}
-    
+
+	@RequestMapping(value="/adduser", produces="application/json")
+	@ResponseBody
+	public ResponseEntity<Object> adduserRest(HttpMethod method, HttpServletRequest request,
+	    HttpServletResponse response, String user, String password) throws URISyntaxException
+	{
+		String url = getHttpBasicServiceProperties().getProperty("adduserURL");
+		System.out.println(LogFormatter.format(className, "adduserRest", "calling "+url+" user:admin password: ---"));
+	    URI uri = new URI(url);
+
+		String geoserverAdminUser = getHttpBasicServiceProperties().getProperty("geoserverAdminUser");
+		String geoserverAdminPassword = getHttpBasicServiceProperties().getProperty("geoserverAdminPassword");
+        
+		ResponseEntity<String> responseEntity = null;
+		HttpHeaders headers = null;
+		try {
+			headers = createHeaders(geoserverAdminUser, geoserverAdminPassword);
+			//headers.setAccept(Arrays.asList(new MediaType[] { MediaType.APPLICATION_XML }));
+			headers.setAccept(Arrays.asList(new MediaType[] { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.TEXT_HTML, MediaType.TEXT_PLAIN }));
+			headers.setContentType(MediaType.APPLICATION_XML);
+
+	        String xmlString = "<?xml version=\"1.0\" encoding=\"utf-8\"?><org.geoserver.rest.security.xml.JaxbUser><userName>" + user +
+	        		"</userName><password>" + password +
+	        		"</password><enabled>true</enabled></org.geoserver.rest.security.xml.JaxbUser>";
+	        HttpEntity<String> requestEntity = new HttpEntity<String>(xmlString, headers);
+
+	        //responseEntity = restTemplate.postForEntity(uri, requestEntity, String.class);
+	        //responseEntity = restTemplate.exchange(uri, HttpMethod.POST, requestEntity, String.class);	
+	        responseEntity = restTemplate.postForEntity(uri, requestEntity, null);	
+		System.out.println(LogFormatter.format(className, "adduserRest", "status code: "+responseEntity.getStatusCode()));
+		} catch (HttpStatusCodeException e) {
+			String msg = e.getMessage();
+		    int statusCode = e.getStatusCode().value();
+	        return new ResponseEntity<Object>(msg,HttpStatus.valueOf(statusCode));
+		} catch (ResourceAccessException e) {
+			e.printStackTrace();
+			try {
+				ResponseEntity<Object> responseEntityRoles = listrolesRest(method, request, response, user);
+				if (responseEntityRoles.getStatusCode()==HttpStatus.OK) {
+			        return new ResponseEntity<Object>("user yet present",HttpStatus.FOUND);
+				}
+		        return new ResponseEntity<Object>("message: "+e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+			} catch (RestClientException ex) {
+				ex.printStackTrace();
+		        return new ResponseEntity<Object>("message: "+ex.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+			}
+		} catch (RestClientException e) {
+			e.printStackTrace();
+	        return new ResponseEntity<Object>("message: "+e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+        return new ResponseEntity<Object>(responseEntity, responseEntity.getStatusCode());
+
+	}
+
+	@RequestMapping(value="/edituser", produces="application/json")
+	@ResponseBody
+	public ResponseEntity<Object> edituserRest(HttpMethod method, HttpServletRequest request,
+	    HttpServletResponse response, String user, String password) throws URISyntaxException
+	{
+		String url = getHttpBasicServiceProperties().getProperty("edituserURL") + "/" + user + "/";
+		System.out.println(LogFormatter.format(className, "edituserRest", "calling "+url+" user:admin password: ---"));
+	    URI uri = new URI(url);
+
+		String geoserverAdminUser = getHttpBasicServiceProperties().getProperty("geoserverAdminUser");
+		String geoserverAdminPassword = getHttpBasicServiceProperties().getProperty("geoserverAdminPassword");
+        
+		ResponseEntity<String> responseEntity = null;
+		HttpHeaders headers = null;
+		try {
+			headers = createHeaders(geoserverAdminUser, geoserverAdminPassword);
+			headers.setAccept(Arrays.asList(new MediaType[] { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.TEXT_HTML, MediaType.TEXT_PLAIN }));
+			headers.setContentType(MediaType.APPLICATION_XML);
+	        String xmlString = "<?xml version=\"1.0\" encoding=\"utf-8\"?><org.geoserver.rest.security.xml.JaxbUser><userName>" + user +
+	        		"</userName><password>" + password +
+	        		"</password><enabled>true</enabled></org.geoserver.rest.security.xml.JaxbUser>";
+	        HttpEntity<String> requestEntity = new HttpEntity<String>(xmlString, headers);
+
+	        //responseEntity = restTemplate.postForEntity(uri, requestEntity, String.class);
+	        //responseEntity = restTemplate.exchange(uri, HttpMethod.POST, requestEntity, Void.class);	
+	        responseEntity = restTemplate.postForEntity(uri, requestEntity, null);	
+		System.out.println(LogFormatter.format(className, "adduserRest", "status code: "+responseEntity.getStatusCode()));
+		} catch (HttpStatusCodeException e) {
+			String msg = e.getMessage();
+		    int statusCode = e.getStatusCode().value();
+	        return new ResponseEntity<Object>(msg,HttpStatus.valueOf(statusCode));
+		} catch (ResourceAccessException e) {
+			e.printStackTrace();
+	        return new ResponseEntity<Object>("user not found: "+user,HttpStatus.NOT_FOUND);
+		} catch (RestClientException e) {
+			e.printStackTrace();
+			String msg = e.getMessage();
+	        return new ResponseEntity<Object>(msg + "\nmessage: "+e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+        return new ResponseEntity<Object>(responseEntity, responseEntity.getStatusCode());
+
+	}
+
+	@RequestMapping(value="/deleteuser", produces="application/json")
+	@ResponseBody
+	public ResponseEntity<Object> deleteuserRest(HttpMethod method, HttpServletRequest request,
+	    HttpServletResponse response, String user) throws URISyntaxException
+	{
+		String url = getHttpBasicServiceProperties().getProperty("deleteuserURL") + "/" + user + "/";
+		System.out.println(LogFormatter.format(className, "deleteuserRest", "calling "+url+" user:admin password: ---"));
+	    URI uri = new URI(url);
+
+		String geoserverAdminUser = getHttpBasicServiceProperties().getProperty("geoserverAdminUser");
+		String geoserverAdminPassword = getHttpBasicServiceProperties().getProperty("geoserverAdminPassword");
+        
+		ResponseEntity<Void> responseEntity = null;
+		try {
+			HttpHeaders headers = createHeaders(geoserverAdminUser, geoserverAdminPassword);			
+			headers.setAccept(Arrays.asList(new MediaType[] { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.TEXT_HTML, MediaType.TEXT_PLAIN }));
+			restTemplate.exchange(url, HttpMethod.DELETE, new HttpEntity<Object>(headers), Void.class);
+		} catch (HttpStatusCodeException e) {
+			String msg = e.getMessage();
+		    int statusCode = e.getStatusCode().value();
+	        return new ResponseEntity<Object>(msg,HttpStatus.valueOf(statusCode));
+		} catch (RestClientException e) {
+			e.printStackTrace();
+			String msg = e.getMessage();
+	        return new ResponseEntity<Object>(msg + "\nmessage: "+e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+        return new ResponseEntity<Object>(responseEntity, responseEntity.getStatusCode());
+
+	}
+
+	@RequestMapping(value="/addrole", produces="application/json")
+	@ResponseBody
+	public ResponseEntity<Object> addroleRest(HttpMethod method, HttpServletRequest request,
+	    HttpServletResponse response, String user, String role) throws URISyntaxException
+	{
+		String url = getHttpBasicServiceProperties().getProperty("addroleURL") + "/" + role + "/user/" + user + "/";
+		System.out.println(LogFormatter.format(className, "addroleRest", "calling "+url+" user:admin password: ---"));
+	    URI uri = new URI(url);
+
+		String geoserverAdminUser = getHttpBasicServiceProperties().getProperty("geoserverAdminUser");
+		String geoserverAdminPassword = getHttpBasicServiceProperties().getProperty("geoserverAdminPassword");
+        
+		ResponseEntity<String> responseEntity = null;
+		try {
+			HttpHeaders headers = createHeaders(geoserverAdminUser, geoserverAdminPassword);			
+			headers.setAccept(Arrays.asList(new MediaType[] { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.TEXT_HTML, MediaType.TEXT_PLAIN }));
+			//responseEntity = restTemplate.exchange(uri, HttpMethod.POST, new HttpEntity<Object>(headers), String.class);
+		    responseEntity = restTemplate.postForEntity(uri, new HttpEntity<Object>(headers), null);	
+		} catch (HttpStatusCodeException e) {
+			String msg = e.getMessage();
+		    int statusCode = e.getStatusCode().value();
+	        return new ResponseEntity<Object>(msg,HttpStatus.valueOf(statusCode));
+		} catch (RestClientException e) {
+			e.printStackTrace();
+			String msg = e.getMessage();
+	        return new ResponseEntity<Object>(msg + "\nmessage: "+e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+        return new ResponseEntity<Object>(responseEntity, responseEntity.getStatusCode());
+
+	}
+	
+	@RequestMapping(value="/deleterole", produces="application/json")
+	@ResponseBody
+	public ResponseEntity<Object> deleteroleRest(HttpMethod method, HttpServletRequest request,
+	    HttpServletResponse response, String user, String role) throws URISyntaxException
+	{
+		String url = getHttpBasicServiceProperties().getProperty("deleteroleURL") + "/" + role + "/user/" + user + "/";
+		System.out.println(LogFormatter.format(className, "deleteroleURL", "calling "+url+" user:admin password: ---"));
+	    URI uri = new URI(url);
+
+		String geoserverAdminUser = getHttpBasicServiceProperties().getProperty("geoserverAdminUser");
+		String geoserverAdminPassword = getHttpBasicServiceProperties().getProperty("geoserverAdminPassword");
+        
+		ResponseEntity<Void> responseEntity = null;
+		try {
+			HttpHeaders headers = createHeaders(geoserverAdminUser, geoserverAdminPassword);			
+			headers.setAccept(Arrays.asList(new MediaType[] { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.TEXT_HTML, MediaType.TEXT_PLAIN }));
+			responseEntity = restTemplate.exchange(uri, HttpMethod.DELETE, new HttpEntity<Object>(headers), Void.class);
+		} catch (HttpStatusCodeException e) {
+			String msg = e.getMessage();
+		    int statusCode = e.getStatusCode().value();
+	        return new ResponseEntity<Object>(msg,HttpStatus.valueOf(statusCode));
+		} catch (RestClientException e) {
+			e.printStackTrace();
+			String msg = e.getMessage();
+	        return new ResponseEntity<Object>(msg + "\nmessage: "+e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+        return new ResponseEntity<Object>(responseEntity, responseEntity.getStatusCode());
+
+	}
+	
+	@RequestMapping(value="/listroles", produces="application/json")
+	@ResponseBody
+	public ResponseEntity<Object> listrolesRest(HttpMethod method, HttpServletRequest request,
+	    HttpServletResponse response, String user) throws URISyntaxException
+	{
+		String url = getHttpBasicServiceProperties().getProperty("listrolesURL") + "/" + user + "/";
+		System.out.println(LogFormatter.format(className, "listrolesRest", "calling "+url+" user:admin password: ---"));
+	    URI uri = new URI(url);
+
+		String geoserverAdminUser = getHttpBasicServiceProperties().getProperty("geoserverAdminUser");
+		String geoserverAdminPassword = getHttpBasicServiceProperties().getProperty("geoserverAdminPassword");
+        
+		ResponseEntity<String> responseEntity = null;
+		try {
+			HttpHeaders headers = createHeaders(geoserverAdminUser, geoserverAdminPassword);			
+			headers.setAccept(Arrays.asList(new MediaType[] { MediaType.APPLICATION_XML }));
+			responseEntity =
+					restTemplate.exchange(uri, HttpMethod.GET, new HttpEntity<Object>(headers), String.class);
+		} catch (HttpStatusCodeException e) {
+			String msg = e.getMessage();
+		    int statusCode = e.getStatusCode().value();
+	        return new ResponseEntity<Object>(msg,HttpStatus.valueOf(statusCode));
+		} catch (RestClientException e) {
+			e.printStackTrace();
+			String msg = e.getMessage();
+	        return new ResponseEntity<Object>(msg + "\nmessage: "+e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+        return new ResponseEntity<Object>(responseEntity, responseEntity.getStatusCode());
+
+	}
 }
